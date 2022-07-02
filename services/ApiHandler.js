202b0e1d23650ff6b1a7f26df601960dfb45836a
belendi1999 <97689725+belendi1999@users.noreply.github.com>
2022-07-02 17:20:21 +0200

    API alimentos conectada

--- services/ApiHandler.js
+++ services/ApiHandler.js
@@ -4,11 +4,11 @@ const axios = require('axios');
 class CharactersApi {
   constructor() {
     this.api = axios.create({
-      baseURL: 'https://rickandmortyapi.com/api'
+      baseURL: 'https://api.edamam.com/api/recipes/v2'
     });
   }
 
-  getAllCharacters = () => this.api.get('/character');
+  getAllCharacters = () => this.api.get('?type=public&q=pasta&app_id=d569f124&app_key=fe0597d512f178c092e803981c8dc8a2');
 
 }
 
