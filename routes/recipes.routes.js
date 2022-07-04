f3015f9f7e8865d4cba1c167ae929f8d60c01acb
belendi1999 <97689725+belendi1999@users.noreply.github.com>
2022-07-04 10:33:11 +0200

    api cambiada

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -13,7 +13,7 @@ router.get('/recipes', isLoggedIn, (req, res)=>{
     RecipesAPI
     .getAllRecipes()
     .then((allRecipes) => {
-        res.render('recipes/list', {recipes: allRecipes.data.hits} )
+        res.render('recipes/list', {recipes: allRecipes.data.results} )
     
     })
     .catch(err => console.log(err));
@@ -24,45 +24,39 @@ router.get('/recipes', isLoggedIn, (req, res)=>{
 
 // Busca cada tipo de recta
 
-
+  
 
 router.post("/add-favorite", isLoggedIn ,(req, res) =>{
-const { apiId } = req.body
+    
+    const { apiId } = req.body
+    console.log(req.body)
 
-    Recipe.find({apiId: apiId})
-	.then (RecipArray => {
-		if (RecipArray.length === 0) {
-            Recipe
-                .create({"apiId": apiId})
-                .then(result => {
-                  User
-                    .findByIdAndUpdate(req.user._id,{$push : {favorites : result._id}})
-                    .then(()=>{
-                        res.redirect("/recipes")
-                    })
-                })
-                .catch(err => console.log(err))
-        } else {
-			User
-            .findById(req.user._id)
-            .then((user)=>{
-                if (!user.favorites.includes(charArray[0]._id)){
-                    User
-                    .findByIdAndUpdate(req.user._id,{$push : {favorites : charArray[0]._id}})
-                    .then(()=>{
-                        res.redirect("/recipes")
-                    })
-                }else{res.redirect("/recipes")}
-            })
-            .catch((err)=>{
-            console.log(err)
-            })
-            
-            
-            
-		}
-	}) 
+    
 })
+
+/*app.get('/artist-search', (req, res, next) => {
+    spotifyApi
+    .searchArtists(req.query.artistName)
+    .then((data) =>{
+        const artists = data.body.artists.items;
+        console.log(artists)
+        res.render('artist-search', {artists})
+    })
+    .catch((err) =>
+        console.log ('The error while searching artists occurred: ', err))
+    })
+
+app.get('/albums/:artistId', (req, res, next) =>{
+    spotifyApi
+    .getArtistAlbums(req.params.artistId)
+    .then((data) =>{
+        const findAlbum = data.body.items;
+        console.log (findAlbum)
+        res.render('albums', {findAlbum})
+    })
+    .catch((err) =>
+    console.log ('The error while searching artists occurred: ', err))
+}) */
 
 
 router.post("/delete-favorite",isLoggedIn,(req,res)=>{
